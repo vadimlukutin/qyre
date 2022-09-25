94cc71b93207e669780febc99fb087811430d608
vadimlukutin <vadim.lukutin@gmail.com>
2022-09-25 19:24:56 +0300

    - add resources

---
+++ lib/src/app/extra/resources/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black1 = Color(0xFF111111);
+  static const black3 = Color(0xFF444444);
+  static const black5 = Color(0xFF656565);
+  static const gray6 = Color(0xFF9C9C9C);
+  static const gray9 = Color(0xFFF0F2F5);
+  static const white10 = Color(0xFFFFFFFF);
+  static const blueFaded12 = Color(0xFF87C6F5);
+  static const redBright18 = Color(0xFFEC4E27);
+  static const gradientBlue20b = Color(0xFF3465C3);
+  static const gradientBlue20e = Color(0xFF5785DE);
+  static const gradientRed21b = Color(0xFFEC4E27);
+  static const gradientRed21e = Color(0xFFF47E61);
+  static const gradientPurple22b = Color(0xFF6B34C3);
+  static const gradientPurple22e = Color(0xFF8E5EDB);
+
+  static const boxShadow =
+      BoxShadow(color: Colors.black38, offset: Offset(0, 1), blurRadius: 1.0, spreadRadius: 1.0);
+}
